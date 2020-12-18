ba59ce6658d631f2e6a704b00c2d3d833316f39d
Victor Hok <victorhok@outlook.com>
2020-12-18 11:59:12 -0500

    touch screen animation live test.

--- scripts.js
+++ scripts.js
@@ -9,4 +9,24 @@ function parallax(event) {
     });
 }
 
+function parallaxMobile(event) {
+    this.querySelectorAll('.shape').forEach(shape => {
+        const speed = shape.getAttribute('data-speed');
+
+            const x = (window.innerWidth - event.touches[0].screenX * speed) / 100;
+            const y = (window.innerHeight - event.touches[0].screenY * speed) / 100;
+
+            console.log(event.touches[0].screenX);
+            shape.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        // for(let touch of event.touches) {
+        //     const x = (window.innerWidth - touch.screenX * speed) / 100;
+        //     const y = (window.innerHeight - touch.screenY * speed) / 100;
+    
+        //     console.log(event.screenX);
+        //     shape.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        // }
+    });
+}
+
 document.addEventListener("mousemove", parallax);
+document.addEventListener("touchmove", parallaxMobile);
