8a98c4e0075a2e2c1dbd3cba7ce5181696e6766e
Victor Hok <victorhok@outlook.com>
2020-12-26 22:26:02 -0500

    Active menu highlighting. :snowman:

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// ==================== PARALLAX EFFECT  ====================
 function parallax(event) {
     this.querySelectorAll('.shape').forEach(shape => {
         const speed = shape.getAttribute('data-speed');
@@ -19,6 +20,35 @@ function parallaxMobile(event) {
     });
 }
 
+// ==================== ACTIVE MENU SCROLLING ====================
+
+window.addEventListener('scroll', (event) => {
+    event.preventDefault();
+
+    const menuLink = document.querySelectorAll('.navbar__ul-menu a');
+    const scrollPositionY = window.scrollY;
+
+    menuLink.forEach( (link) => {
+        // Get DOM element by ID.
+        const section = document.querySelector(link.hash);
+
+        // If the current scroll position is somewhere within a specific section.
+        if ( scrollPositionY >= section.offsetTop && scrollPositionY < section.offsetTop + section.offsetHeight ) {
+            link.classList.add('link--active');
+        } else {
+            link.classList.remove('link--active');
+        }
+    })
+});
+
+/* 
+Add event listener depending on whether desktop or mobile device is detected.
+
+Key differences:
+    -finger touch vs. cursor movements
+    -mobile requires increased sensitivity due to smaller screen
+    -on mobile vertical touch motion causes scrolling and little value added for including vertical parallax effect.
+*/
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.addEventListener("touchmove", parallaxMobile);
 } else {
